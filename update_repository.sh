a5c920496aa9a2ef13fe737c76cba78f06639d36
Michael Kopp <kopp.michael@yahoo.de>
2020-07-10 22:15:15 +0200

    pacman -Sy

--- update_repository.sh
+++ update_repository.sh
@@ -6,10 +6,13 @@ echo "AUR Packages requested to install: $INPUT_PACKAGES"
 echo "AUR Packages to fix missing dependencies: $INPUT_MISSING_AUR_DEPENDENCIES"
 echo "AUR Packages to install (including dependencies): $packages_with_aur_dependencies"
 
+# sync repositories
+pacman -Sy
+
 if [ -n "$INPUT_MISSING_PACMAN_DEPENDENCIES" ]
 then
     echo "Additional Pacman packages to install: $INPUT_MISSING_PACMAN_DEPENDENCIES"
-    pacman -Sy $INPUT_MISSING_PACMAN_DEPENDENCIES
+    pacman -S $INPUT_MISSING_PACMAN_DEPENDENCIES
 fi
 
 echo workspace
