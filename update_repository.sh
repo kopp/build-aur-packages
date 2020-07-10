ea789e9fc5e4dc766cb280969feedba373c8cee9
Michael Kopp <kopp.michael@yahoo.de>
2020-07-10 22:42:07 +0200

    remove symlinks

--- update_repository.sh
+++ update_repository.sh
@@ -30,6 +30,7 @@ then
     # make sure that the .db/.files files are in place
     # Note: Symlinks fail to upload, so copy those files
     cd $GITHUB_WORKSPACE
+    rm aurci2.db aurci2.files
     cp aurci2.db.tar.gz aurci2.db
     cp aurci2.files.tar.gz aurci2.files
 fi
